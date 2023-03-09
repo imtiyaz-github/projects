84c0dbc215ddef146ece90519e33c0004967ab9b
imtiyazgithub <imtiyazshirsangi456@gmail.com>
2023-03-09 15:46:55 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,25 @@
+
+
+function clear() {
+    document.getElementById('result').value = "";
+
+}
+function del() {
+    document.getElementById('result').value = "";
+}
+function delet() {
+    document.getElementById('result').value  = " ";
+}
+
+function display (val) {
+    document.getElementById("result").value +=val;
+
+
+}
+
+function equate () {
+    let x  = document.getElementById("result").value;
+    let y   = eval(x);
+    document.getElementById("result").value = y;
+}
+
