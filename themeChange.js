b7130bf01197b6e0561627aee7036adbf1a4e619
imtiyazgithub <imtiyazshirsangi456@gmail.com>
2023-03-13 17:54:32 +0530

    first commit

---
+++ themeChange.js
@@ -0,0 +1,24 @@
+
+var element;
+
+document.getElementById('circle1').addEventListener('click' , function() {
+    document.getElementById('circle1').style.background  ="red";
+});
+
+
+document.getElementById('circle2').addEventListener('click' , function() {
+    document.getElementById('circle2').style.background  ="yellow";
+
+});
+
+
+document.getElementById('circle3').addEventListener('click' , function() {
+    document.getElementById('circle3').style.background  ="black";
+
+});
+
+
+document.getElementById('circle4').addEventListener('click' , function() {
+    document.getElementById('circle4').style.background  ="pink";
+
+});
